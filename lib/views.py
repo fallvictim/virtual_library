6ceb3f7364901cf8d325503f841262a4a88d3701
fall.v <lifeisgrief@gmail.com>
2018-04-16 19:34:44 +0500

    Modified html

--- lib/views.py
+++ lib/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Book
 
-# Create your views here.
+
+def main_page(request):
+    books = Book.objects.order_by('title')
+    return render(request, 'lib/main.html', {'books': books})
