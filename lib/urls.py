c39b1bccb0e04798d88bfd97025ee9d9ebffa02b
fall.v <lifeisgrief@gmail.com>
2018-04-17 16:44:20 +0500

    Added page for book details

--- lib/urls.py
+++ lib/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.main_page, name='main_page'),
+    url(r'^book/(?P<pk>\d+)/$', views.book_detail, name='book_detail')
 ]
--- lib/views.py
+++ lib/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Book
 
 
 def main_page(request):
     books = Book.objects.order_by('title')
     return render(request, 'lib/main.html', {'books': books})
+
+
+def book_detail(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    return render(request, 'lib/book_detail.html', {'book': book})
